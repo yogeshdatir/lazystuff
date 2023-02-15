80a50e5b1e8dae009a17ce3da6616f7187954670
Yogesh Datir <yogdat@gmail.com>
2023-02-15 18:09:14 +0530

    refactored folder structure for multiselect

--- src/components/SelectDropdown/Other_designs/MultiSelectWithCheckbox/MultiSelectWithCheckbox.tsx
+++ src/components/SelectDropdown/Other_designs/MultiSelectWithCheckbox/MultiSelectWithCheckbox.tsx
@@ -1,10 +1,26 @@
 // Tutorial: https://youtu.be/bAJlYgeovlg
 // Repo: https://github.com/WebDevSimplified/react-select
 
-import styled from '@emotion/styled';
 import React, { KeyboardEvent, MouseEvent, useRef, useState } from 'react';
-import { ReactComponent as CheckedIcon } from '../../../assets/icons8-done.svg';
-import useClickOutside from '../../../hooks/useClickOutside';
+import { ReactComponent as CheckedIcon } from '../../../../assets/icons8-done.svg';
+import useClickOutside from '../../../../hooks/useClickOutside';
+import {
+  ActionsWrapper,
+  Caret,
+  CaretWrapper,
+  ClearButton,
+  Container,
+  CustomCheckbox,
+  CustomPlaceholderText,
+  Divider,
+  Option,
+  OptionsBox,
+  SearchInput,
+  SearchWrapper,
+  SelectBox,
+  SelectedOptionBadge,
+  SelectedValueText,
+} from './MultiSelectWithCheckbox.styled';
 
 export type ISelectOption = {
   label: string;
@@ -35,182 +51,10 @@ type IProps = {
   placeholder?: string | number;
 } & (SingleSelectProps | MultiSelectProps);
 
-const Container = styled.div`
-  position: relative;
-  width: 20em;
-  min-height: 1.5em;
-  border: 0.05em solid #777;
-  border-radius: 0.25em;
-  outline: none;
-`;
-
-const SelectBox = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  gap: 0.5em;
-  padding: 0.5em;
-  cursor: pointer;
-  box-sizing: border-box;
-  border-radius: 0.25em;
-
-  :focus,
-  :focus-visible,
-  :focus-within {
-    outline: 1.5px solid hsl(200, 100%, 50%);
-  }
-`;
-
-const SelectedValueText = styled.span`
-  flex-grow: 1;
-  display: flex;
-  gap: 0.5em;
-  flex-wrap: wrap;
-`;
-
-const ActionsWrapper = styled.div`
-  display: flex;
-  margin-left: auto;
-`;
-
-const ClearButton = styled.button`
-  background: none;
-  color: #777;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  padding: 0;
-  font-size: 1.25em;
-  width: 1.5rem;
-  height: 1.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  :focus,
-  :hover {
-    color: #222;
-    scale: 1.1;
-  }
-`;
-
-const Divider = styled.div`
-  background-color: #777;
-  align-self: stretch;
-  width: 0.05em;
-`;
-
-const Caret = styled.div`
-  translate: 0 30%;
-  border: 0.35em solid transparent;
-  border-top-color: #777;
-  cursor: pointer;
-`;
-
-const CaretWrapper = styled.div`
-  width: 1.5rem;
-  height: 1.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  :focus,
-  :hover {
-    div {
-      border-top-color: #222;
-      scale: 1.1;
-    }
-  }
-`;
-
-interface IOptionBox {
-  show: boolean;
-}
-
-const OptionsBox = styled.ul<IOptionBox>`
-  position: absolute;
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  display: ${({ show }) => (show ? 'block' : 'none')};
-  max-height: 15em;
-  overflow-y: auto;
-  border: 0.05em solid #777;
-  border-radius: 0.25em;
-  width: 100%;
-  left: 0;
-  top: calc(100% + 0.25em);
-  background-color: white;
-  z-index: 100;
-`;
-
-interface IOption {
-  isHighlightedIndex?: boolean;
-  isSelected: boolean;
-}
-
-const Option = styled.li<IOption>`
-  padding: 0.25em 0.5em;
-  cursor: pointer;
-  background-color: ${({ isSelected, isHighlightedIndex }) =>
-    isHighlightedIndex ? 'hsl(200, 100%, 50%)' : 'transparent'};
-  color: ${({ isHighlightedIndex }) => (isHighlightedIndex ? 'white' : '')};
-  display: flex;
-  align-items: center;
-  gap: 0.5em;
-`;
-
-const SelectedOptionBadge = styled.button`
-  display: flex;
-  align-items: center;
-  border: 0.05em solid #777;
-  border-radius: 0.25em;
-  padding: 0.15em 0.25em;
-  gap: 0.25em;
-  cursor: pointer;
-  background: none;
-  outline: none;
-
-  :hover,
-  :focus {
-    background-color: hsl(0, 100%, 90%);
-    border-color: hsl(0, 100%, 50%);
-  }
-
-  :hover > .remove-btn,
-  :focus > .remove-btn {
-    color: hsl(0, 100%, 50%);
-  }
-
-  .remove-btn {
-    font-size: 1.25em;
-    color: #777;
-  }
-`;
-
-const SearchWrapper = styled.div`
-  padding: 0.25em 0.5em;
-`;
-
-const SearchInput = styled.input`
-  width: 100%;
-  border-radius: 2px;
-  border: 0.05em solid #777;
-
-  :focus-visible {
-    outline: 1.5px solid hsl(200, 100%, 50%);
-  }
-`;
-
-const CustomPlaceholderText = styled.span`
-  color: gray;
-`;
-
 // TODO: Add select all feature
 // TODO: Add options groups
 // TODO: Add select all for option groups
-// TODO: Add async await
-// TODO: Add empty state option
+// TODO: Add async await - lazy loading
 // TODO: Bug: Pressing enter on clearOption icon clears the selections but if user presses down arrow and selects option then it doesn't work and dropdown is closed
 
 const DEFAULT_PLACEHOLDER_COPY = `Select an option...`;
@@ -356,21 +200,9 @@ const MultiSelectWithCheckbox = ({
             isSelected={isOptionSelected(option)}
             isHighlightedIndex={hoveredIndex === index}
           >
-            <div
-              role="checkbox"
-              aria-checked={false}
-              style={{
-                width: '1rem',
-                height: '1rem',
-                border: '1px solid black',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                overflow: 'hidden',
-              }}
-            >
+            <CustomCheckbox role="checkbox" aria-checked={false}>
               {isOptionSelected(option) && <CheckedIcon />}
-            </div>
+            </CustomCheckbox>
             {option.label}
           </Option>
         );
