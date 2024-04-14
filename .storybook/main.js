4836459dcf77608afe0e7084eac1aa956c2a8d7e
Yogesh Datir <yogdat@gmail.com>
2024-04-14 19:48:22 +0530

    resolved storybook issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,20 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-mdx-gfm',
   ],
 
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-mdx-gfm"
-  ],
-
-  "framework": {
-    name: "@storybook/react-webpack5",
-    options: {}
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
   },
 
   docs: {
-    autodocs: true
-  }
-}
+    autodocs: true,
+  },
+};
